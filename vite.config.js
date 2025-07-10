cf434fc0059b2a5d200836ad14fe051f598e2699
Shubh-Patil31 <shubhamavp123@gmail.com>
2025-07-10 23:12:46 +0530

    Merge remote changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/feedback_form/", // 👈 Add this line
+  base: "/feedback_form/",
 });
